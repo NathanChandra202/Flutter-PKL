c2315683deb76dee5ee15c706786fa474d65871d
unknown <aliffioaliffio4@gmail.com>
2026-09-21 09:53:19 +0700

    update fitur forget password

--- backend/app/api/endpoints/auth.py
+++ backend/app/api/endpoints/auth.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
